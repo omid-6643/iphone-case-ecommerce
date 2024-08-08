a2f41bf2ca7e6033c437b45551981fe893984873
omid-6643 <o.beygi@gmail.com>
2024-08-08 12:19:49 +0330

    feat (main-page) : add review part

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,7 @@
 import { Icons } from "@/components/Icons";
 import MaxWidthWrapper from "@/components/MaxWidthWrapper";
 import Phone from "@/components/Phone";
+import Reviews from "@/components/Reviews";
 import { Check, Star } from "lucide-react";
 
 const images = [
@@ -109,7 +110,7 @@ export default function Home() {
               alt="snake"
             />
           </div>
-          <div className="mx-auto gird max-w-2xl grid-cols-1 px-4 lg:mx-0 lg:max-w-none lg:grid-cols-2 gap-y-16">
+          <div className="mx-auto grid max-w-2xl grid-cols-1 px-4 lg:mx-0 lg:max-w-none lg:grid-cols-2 gap-y-16">
             <div className="flex flex-auto flex-col gap-4 lg:pr-8 xl:pr-20">
               <div className="flex gap-0.5 mb-2">
                 <Star className="size-5 text-green-600 fill-green-600" />
@@ -144,8 +145,46 @@ export default function Home() {
                 </div>
               </div>
             </div>
+            <div className="flex flex-auto flex-col gap-4 lg:pr-8 xl:pr-20">
+              <div className="flex gap-0.5 mb-2">
+                <Star className="size-5 text-green-600 fill-green-600" />
+                <Star className="size-5 text-green-600 fill-green-600" />
+                <Star className="size-5 text-green-600 fill-green-600" />
+                <Star className="size-5 text-green-600 fill-green-600" />
+                <Star className="size-5 text-green-600 fill-green-600" />
+              </div>
+              <div className="text-lg leading-8">
+                <p>
+                  &quot;I usually keep my phone together with my keys in my
+                  pocket and that led to some pretty heavy scratch marks on all
+                  of my last phone cases. this one, besides a barely noticeable
+                  scratch on corner,
+                  <span className="p-0.5 bg-slate-800 text-white">
+                    looks brand new after about half a year
+                  </span>
+                  . I dig it.&quot;
+                </p>
+              </div>
+              <div className="flex gap-4 mt-2">
+                <img
+                  src="/users/user-4.jpg"
+                  alt="user"
+                  className="rounded-full size-12 object-cover"
+                />
+                <div className="flex flex-col">
+                  <p className="font-semibold">Omid</p>
+                  <div className="flex gap-1.5 items-center text-zinc-600">
+                    <Check className="size-4 stroke-[3px] text-green-600" />
+                    <p className="text-sm">Verified Purchase</p>
+                  </div>
+                </div>
+              </div>
+            </div>
           </div>
         </MaxWidthWrapper>
+        <div className="pt-16">
+          <Reviews />
+        </div>
       </section>
     </div>
   );
