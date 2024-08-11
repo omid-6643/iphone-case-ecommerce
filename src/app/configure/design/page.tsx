59da264cea5d7beb6ee8c0c42934bb8b74f68427
omid-6643 <o.beygi@gmail.com>
2024-08-11 17:02:27 +0330

    feat (configure/upload) : add design pgae

--- src/app/configure/design/page.tsx
+++ src/app/configure/design/page.tsx
@@ -1,3 +1,7 @@
+import { db } from "@/db";
+import { notFound } from "next/navigation";
+import DesignConfigurator from "./DesignConfigurator";
+
 interface PageProps {
   searchParams: {
     [key: string]: string | string[] | undefined;
@@ -7,7 +11,25 @@ interface PageProps {
 const Page = async ({ searchParams }: PageProps) => {
   const { id } = searchParams;
 
-  return <div>Page</div>;
+  if (!id || typeof id !== "string") {
+    return notFound();
+  }
+
+  const configuration = await db.configuration.findUnique({ where: { id } });
+
+  if (!configuration) {
+    return notFound();
+  }
+
+  const { imageUrl, width, height, id: configurationId} = configuration;
+
+  return (
+    <DesignConfigurator
+      imageDimensions={{ width, height }}
+      imageUrl={imageUrl}
+      configId={configurationId}
+    />
+  );
 };
 
 export default Page;
