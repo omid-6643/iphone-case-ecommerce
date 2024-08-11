88d91d876db9c44e2d8ef120d1f261416c1c74db
omid-6643 <o.beygi@gmail.com>
2024-08-11 12:56:29 +0330

    feat (configure/upload) : add db

---
+++ src/app/configure/design/page.tsx
@@ -0,0 +1,13 @@
+interface PageProps {
+  searchParams: {
+    [key: string]: string | string[] | undefined;
+  };
+}
+
+const Page = async ({ searchParams }: PageProps) => {
+  const { id } = searchParams;
+
+  return <div>Page</div>;
+};
+
+export default Page;
